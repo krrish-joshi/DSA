d110330d8b53c086f8d0d3b5d4f95a906c2755e4
Krrish Joshi <24bai70093@cuchd.in>
2026-07-18 11:16:15 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (35.05%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,40 +1,40 @@
+    //     int n = nums.size();
+    //     for (int i = 0; i < n; i++){
+    //         if(nums[i] == target){
+    //             return i;
+    //         }
+    //     } 
+    //         return -1;
+    // }
 
 
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        for (int i = 0; i < n; i++){
-            if(nums[i] == target){
-                return i;
+    int search(vector<int>& A, int target) {
+        int st = 0, end = A.size() - 1;
+
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+
+            if (A[mid] == target) {
+                return mid;
             }
-        } 
-            return -1;
+
+            if (A[st] <= A[mid]) { // left half is sorted
+                if (A[st] <= target && target <= A[mid]) {
+                    end = mid - 1;
+                } else {
+                    st = mid + 1;
+                }
+            } else { // right half is sorted
+                if (A[mid] <= target && target <= A[end]) {
+                    st = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
     }
-    //     int st = 0, end = A.size() - 1;
-
-    //     while (st <= end) {
-    //         int mid = st + (end - st) / 2;
-
-    //         if (A[mid] == target) {
-    //             return mid;
-    //         }
-
-    //         if (A[st] <= A[mid]) { // left half is sorted
-    //             if (A[st] <= target && target <= A[mid]) {
-    //                 end = mid - 1;
-    //             } else {
-    //                 st = mid + 1;
-    //             }
-    //         } else { // right half is sorted
-    //             if (A[mid] <= target && target <= A[end]) {
-    //                 st = mid + 1;
-    //             } else {
-    //                 end = mid - 1;
-    //             }
-    //         }
-    //     }
-
-    //     return -1;
-    // }
 };
